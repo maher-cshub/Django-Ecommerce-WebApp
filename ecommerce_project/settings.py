d70bb0acd195ae1b022585fb40bae97bb9822954
maher-cshub <maherarab.1299@gmail.com>
2024-10-04 19:45:41 +0100

    set the project frontend template

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -119,13 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 
-STATICFILES_DIR = [os.path.join(BASE_DIR, "staticfiles")]
-
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
