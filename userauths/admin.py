ec3f22ee7ffcb8064286ef1b7fb14bdf2cb16cb8
maher-cshub <maherarab.1299@gmail.com>
2024-10-06 23:41:49 +0100

    created a simple user register form

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from userauths.models import User
-from .models import User
 
 
 # Register your models here.
--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from userauths.forms import UserRegisterForm
 # Create your views here.
+
+def register_view(request):
+    form = UserRegisterForm()
+    #context
+    context = {
+        "register_form":form
+    }
+    return render(request,"userauths/register.html",context=context)
+
+
+
+
